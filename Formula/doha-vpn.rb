7ca34f1eab3cea90891810e668aa66a112cc0c2a
George S <georgestribley@outlook.com>
2025-11-17 06:50:12 -0500

    Initial brew tap commit for doha-vpn

--- Formula/doha-vpn.rb
+++ Formula/doha-vpn.rb
@@ -2,7 +2,7 @@ class DohaVpn < Formula
   desc "Doha Office VPN Update"
   homepage "https://github.com/bcg-doha/"
   url "https://github.com/bcg-doha/homebrew-vpn-config/releases/download/v1.3/doha-vpn-1.3.tar.gz"
-  sha256 "cc63d2f55f8ae0aac3dbd137b27752dd737ab1e40f808a2cf1fc243852e66ad9"
+  sha256 "d56b9e0b265f016ab8967ed0c59eda571adab0def03016ee54b8a3e0d79826bd"
   version "1.3"
   depends_on "python@3.12"
 
